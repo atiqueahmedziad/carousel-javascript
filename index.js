75ba8934d90dd64b06efcc890d2f6f492ad8fc8a
atiqueahmedziad <softfilebd@gmail.com>
2020-06-06 22:42:55 +0600

    carousel layout v4 added

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
+// Ziad Product Carousel
+
 document.addEventListener("DOMContentLoaded", event => {
   const itemClassName = "carousel-product";
   let items = document.getElementsByClassName(itemClassName),
-      totalItems = items.length,
-      slide = 0,
-      moving = true;
+    totalItems = items.length,
+    slide = 0,
+    moving = true;
+
+  const concernSubHeading = document.querySelectorAll('.subheading-concern .no-slider-item');
+  const concernProducts = document.querySelectorAll('.products-concern .no-slider-item');
+  const priceProduct = document.querySelectorAll('.price-carousel-product .no-slider-item');
+  const learnHow = document.querySelectorAll('.learn-how-link .no-slider-item');
+  const subHeadingClsName = 'no-slider-item';
 
   function setInitialClasses() {
-    items[totalItems - 1].classList.add("prev");
+    learnHow[totalItems - 1].classList.add("previous-product");
+    learnHow[0].classList.add("initial");
+    learnHow[0].classList.add("active");
+    learnHow[1].classList.add("next-product");
+
+    priceProduct[totalItems - 1].classList.add("previous-product");
+    priceProduct[0].classList.add("initial");
+    priceProduct[0].classList.add("active");
+    priceProduct[1].classList.add("next-product");
+
+    concernProducts[totalItems - 1].classList.add("previous-product");
+    concernProducts[0].classList.add("initial");
+    concernProducts[0].classList.add("active");
+    concernProducts[1].classList.add("next-product");
+
+    concernSubHeading[totalItems - 1].classList.add("previous-product");
+    concernSubHeading[0].classList.add("initial");
+    concernSubHeading[0].classList.add("active");
+    concernSubHeading[1].classList.add("next-product");
+
+    items[totalItems - 1].classList.add("previous-product");
+    items[0].classList.add("initial");
     items[0].classList.add("active");
-    items[1].classList.add("next");
+    items[1].classList.add("next-product");
   }
 
   function setEventListeners() {
@@ -30,8 +59,7 @@ document.addEventListener("DOMContentLoaded", event => {
   function moveCarouselTo(slide) {
     if(!moving) {
       disableInteraction();
-
-      var newPrevious = slide - 1,
+      let newPrevious = slide - 1,
       newNext = slide + 1;
 
       if(newNext === totalItems) {
@@ -42,9 +70,25 @@ document.addEventListener("DOMContentLoaded", event => {
         newPrevious = totalItems - 1;
       }
 
-      items[newPrevious].className = itemClassName + " prev";
+      learnHow[newPrevious].className = subHeadingClsName + " previous-product";
+      learnHow[slide].className = subHeadingClsName + " active";
+      learnHow[newNext].className = subHeadingClsName + " next-product";
+
+      priceProduct[newPrevious].className = subHeadingClsName + " previous-product";
+      priceProduct[slide].className = subHeadingClsName + " active";
+      priceProduct[newNext].className = subHeadingClsName + " next-product";
+
+      concernProducts[newPrevious].className = subHeadingClsName + " previous-product";
+      concernProducts[slide].className = subHeadingClsName + " active";
+      concernProducts[newNext].className = subHeadingClsName + " next-product";
+
+      concernSubHeading[newPrevious].className = subHeadingClsName + " previous-product";
+      concernSubHeading[slide].className = subHeadingClsName + " active";
+      concernSubHeading[newNext].className = subHeadingClsName + " next-product";
+
+      items[newPrevious].className = itemClassName + " previous-product";
       items[slide].className = itemClassName + " active";
-      items[newNext].className = itemClassName + " next";
+      items[newNext].className = itemClassName + " next-product";
     }
   }
 
