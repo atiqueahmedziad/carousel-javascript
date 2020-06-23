a1348af5e9e754d6b15d67bd2e485c27c2d71b7a
atiqueahmedziad <softfilebd@gmail.com>
2020-06-23 15:36:01 +0600

    testimonial slider

--- main.js
+++ main.js
@@ -1,38 +1,23 @@
-// Ziad Product Carousel
-
 document.addEventListener("DOMContentLoaded", event => {
-  const itemClassName = "single-product-slider";
+  const itemClassName = "motif-single-testimonial";
   let items = document.getElementsByClassName(itemClassName),
     totalItems = items.length,
     slide = 0,
-    moving = true,
-    indicatorWrapper = document.getElementById('slider-indicator-wrapper');
+    moving = true;
 
-  for(let i=0; i<totalItems; i++) {
-    indicatorWrapper.insertAdjacentHTML('beforeend', `<div id=${i} class="slider-pointer"></div>`);
-  }
-
-  const indicators = document.getElementsByClassName('slider-pointer');
-  console.log(indicators);
-  const indicatorsArray = Array.from(indicators);
-  indicatorsArray.forEach(indicator => {
-    indicator.addEventListener('click', (e)=> {
-      e.preventDefault();
-      moveToSlide(Number(indicator.id));
-    });
-  });
+  const isSliderEnable = totalItems > 1;
 
   function setInitialClasses() {
-    indicators[0].classList.add('active-slider');
     items[0].classList.add("active");
   }
 
   function setEventListeners() {
-    const next = document.getElementById('product-slider-btn-next'),
-        prev = document.getElementById('product-slider-btn-prev');
-
-    next.addEventListener('click', moveNext);
-    prev.addEventListener('click', movePrev);
+    const next = document.getElementById('testimonial-slider-btn-next'),
+        prev = document.getElementById('testimonial-slider-btn-prev');
+    if(isSliderEnable) {
+      next.addEventListener('click', moveNext);
+      prev.addEventListener('click', movePrev);
+    }
   }
 
   function disableInteraction() {
@@ -50,11 +35,12 @@ document.addEventListener("DOMContentLoaded", event => {
 
       slide = toSlide;
 
-      indicators[prev].classList.remove('active-slider');
-      indicators[toSlide].classList.add('active-slider');
       items[toSlide].classList.add('active');
       items[prev].classList.remove('active');
-      timer = setInterval(() => moveNext(), timerInterval);
+
+      if(isSliderEnable) {
+      	timer = setInterval(() => moveNext(), timerInterval);
+      }
     }
   }
 
@@ -90,64 +76,62 @@ document.addEventListener("DOMContentLoaded", event => {
     setInitialClasses();
     setEventListeners();
 
-    //timer = setInterval(() => moveNext(), timerInterval);
+    if(isSliderEnable) {
+      	timer = setInterval(() => moveNext(), timerInterval);
+    }
     moving = false;
   }
 
-  let isDown = false;
-  const slider = document.querySelector('.product-slider-list');
-  let startX;
-  let scrollLeft;
-  const screenWidth = window.screen.width;
-  let dragValue = 350;
+  if(isSliderEnable) {
 
-  if(screenWidth < 950) {
-    dragValue = 180;
-  }
-  else if(screenWidth < 500) {
-    dragValue = 100;
-  }
+    let isDown = false;
+    const slider = document.querySelector('.motif-testimonial-list');
+    let startX;
+    let scrollLeft;
+    const screenWidth = window.screen.width;
+    let dragValue = 350;
 
-  slider.addEventListener('mousedown', (e) => {
-    isDown = true;
-    slider.classList.add('super-active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-  });
-
-  slider.addEventListener('mouseleave', () => {
-    isDown = false;
-    slider.classList.remove('super-active');
-  });
-
-  slider.addEventListener('mouseup', () => {
-    isDown = false;
-    slider.classList.remove('super-active');
-  });
-
-  slider.addEventListener('mousemove', (e) => {
-    if(!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX)*3;
-    slider.scrollLeft = scrollLeft - walk;
-    console.log(walk);
-    if(walk > dragValue) {
-      movePrev();
+    if(screenWidth < 950) {
+      dragValue = 180;
     }
-    else if(walk < -dragValue) {
-      moveNext();
+    else if(screenWidth < 500) {
+      dragValue = 100;
     }
-  });
+
+    slider.addEventListener('mousedown', (e) => {
+      isDown = true;
+      slider.classList.add('super-active');
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove('super-active');
+    });
+
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove('super-active');
+    });
+
+    slider.addEventListener('mousemove', (e) => {
+      if(!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX)*3;
+      slider.scrollLeft = scrollLeft - walk;
+      console.log(walk);
+      if(walk > dragValue) {
+        movePrev();
+      }
+      else if(walk < -dragValue) {
+        moveNext();
+      }
+    });
+  }
 
   const timerInterval = 3000;
 
   initCarousel();
-
-  // const navigator = document.getElementsByClassName('product-slider-navigator')[0];
-  // const navigatorWidth = navigator.clientWidth;
-  // const leftWidth = "calc(50% - " + navigatorWidth/2 + "px)";
-  // if(screen.width < 999) {
-  //   navigator.style.left = leftWidth;
-  // }
 });
